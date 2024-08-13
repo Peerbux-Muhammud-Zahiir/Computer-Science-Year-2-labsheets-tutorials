1e549221a2e19a6c6668cdf77696e668d051c3c3
Peerbux-Muhammud-Zahiir <zahiirpeerbux2019@gmail.com>
2024-08-14 00:18:32 +0400

    Add shape.c

---
+++ ICT2022Y - Operating Systems 2024/C projects/shape.c/main.c
@@ -0,0 +1,30 @@
+/*
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    printf("\n");
+    printf("   *\n");
+    printf("  /|\n");
+    printf(" / |\n");
+    printf("/__|\n");
+
+
+return 0;
+}
