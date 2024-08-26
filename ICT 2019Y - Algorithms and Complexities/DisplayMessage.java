92e7960e36267e1a9e4618bd33d5213b9b09e1ab
Zahiir Peerbux <zahiirpeerbux2019@gmail.com>
2024-08-26 23:56:58 +0400

    Create DisplayMessage.java

---
+++ ICT 2019Y - Algorithms and Complexities/DisplayMessage.java
@@ -0,0 +1,14 @@
+public class DisplayMessage{
+    public static void main(String[] args){
+        System.out.print("Hello world\n");
+        System.out.println("I am Zahiir");
+        System.out.println(2024);
+        System.out.println(4*5);
+        System.out.println(10%2);
+        System.out.println(100/25);
+    }
+}
+
+
+
+
