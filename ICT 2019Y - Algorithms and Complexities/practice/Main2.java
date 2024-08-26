877ba446b4543bcaaf91c59474220ff3676ae28a
Peerbux-Muhammud-Zahiir <zahiirpeerbux2019@gmail.com>
2024-08-27 02:17:57 +0400

    new commit

---
+++ ICT 2019Y - Algorithms and Complexities/practice/Main2.java
@@ -0,0 +1,14 @@
+public class Main2 {
+    public static void main(String[] args){
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+    }
+}
+
+
+//  javac Main2.java
+//  java Main2 hello world zahiir
+// hello
+// world
+// zahiir
---
+++ ICT 2019Y - Algorithms and Complexities/practice/Me.java
@@ -0,0 +1,8 @@
+public class Me {
+    public static void sayName(){
+        System.out.println("Hello Zahiir");
+    }
+    public static void sayAge() {
+        System.out.println("21");
+    }
+}
