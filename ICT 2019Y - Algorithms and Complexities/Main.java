6aaf56578efc52a4ae427bac88c1651b4b1b8b78
Zahiir Peerbux <zahiirpeerbux2019@gmail.com>
2024-08-27 01:54:55 +0400

    Create Main.java

---
+++ ICT 2019Y - Algorithms and Complexities/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
+
+
+// javac Main.java
+// java Main Hello world
