e8be6c588f06eddd93a21600f82beb2f2c2cb74e
Zahiir Peerbux <zahiirpeerbux2019@gmail.com>
2024-08-27 01:04:02 +0400

    Create Zahiir.java

---
+++ ICT 2019Y - Algorithms and Complexities/Zahiir.java
@@ -0,0 +1,20 @@
+public class Zahiir {
+    public static void main(String[] args) {
+        sayHi();
+        //this is a single line comment
+        System.out.println("Welcome to java!!!");
+        sayHi();
+        /*
+        This
+        is
+        a
+        multi
+        line
+        comment
+        */
+    }
+
+    public static void sayHi() {
+        System.out.println("Hi there!");
+    }
+}
