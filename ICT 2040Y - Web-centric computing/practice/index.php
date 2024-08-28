900c05c07b21961de8f15592a00acf8450bdbabd
Zahiir Peerbux <zahiirpeerbux2019@gmail.com>
2024-08-29 00:52:09 +0400

    Create index.php

---
+++ ICT 2040Y - Web-centric computing/practice/index.php
@@ -0,0 +1,28 @@
+<?php
+echo"Hello\n";
+echo"My name is Zahiir";
+// single line comment
+
+/*
+multi line comment
+multi line comment
+*/
+$food="pasta";
+$name="Zahiir";
+$email="zahiir@fake.com";
+echo "\n\n$name likes $food\n";
+echo "Your email is $email";
+$money=100;
+echo "\nYou have \$$money"; 
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Button</title>
+</head>
+<body>
+<button>Click me</button>
+</body>
+</html>
