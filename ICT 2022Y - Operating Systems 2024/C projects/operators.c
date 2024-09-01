e64cee783ea562450ecf13e70e620eff00eb3f5f
Zahiir Peerbux <zahiirpeerbux2019@gmail.com>
2024-09-01 14:09:44 +0400

    Update operators.c

--- ICT 2022Y - Operating Systems 2024/C projects/operators.c
+++ ICT 2022Y - Operating Systems 2024/C projects/operators.c
@@ -8,7 +8,7 @@ int main(){
     printf("Enter another number: ");
     scanf("%d",&b);
     printf("%d / %d = %d\n",a,b,a/b);
-    printf("%d mod %d = %d",a,b,a%b);
+    printf("%d %% %d = %d",a,b,a%b);
     return 0;
 }
 
