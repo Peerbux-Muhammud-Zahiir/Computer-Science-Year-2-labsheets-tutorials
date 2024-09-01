4d92683bc48acf895922f151b3d2d66ec3487755
Zahiir Peerbux <zahiirpeerbux2019@gmail.com>
2024-09-01 14:20:57 +0400

    Create incrementOp.c

---
+++ ICT 2022Y - Operating Systems 2024/C projects/incrementOp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a,b;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    printf("Enter another number: ");
+    scanf("%d",&b);
+    printf("%d\n",a+++b);
+    printf("%d\n",a);
+    printf("%d\n",b);
+    printf("%d\n",++a+b);
+    printf("%d\n",a);
+    printf("%d\n",b);
+
+    return 0;
+}
+
+
+
+
