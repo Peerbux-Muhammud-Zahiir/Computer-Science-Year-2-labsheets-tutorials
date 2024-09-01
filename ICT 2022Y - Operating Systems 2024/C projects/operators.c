adc1f93385b73770184f055a69e072e84000a830
Zahiir Peerbux <zahiirpeerbux2019@gmail.com>
2024-09-01 14:06:22 +0400

    Create operators.c

---
+++ ICT 2022Y - Operating Systems 2024/C projects/operators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a,b;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    printf("Enter another number: ");
+    scanf("%d",&b);
+    printf("%d / %d = %d\n",a,b,a/b);
+    printf("%d mod %d = %d",a,b,a%b);
+    return 0;
+}
+
+
+
+
+//  +,×,÷,/  = arithmetic operators
+//  >,<,>=   = relational operators
+//  AND,OR   = logical operators
